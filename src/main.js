8241681d28483d2a578107ec50173bfebd06dd8b
VictorAp06 <victor.p02@hotmail.com>
2021-10-23 04:58:59 -0300

    Inicio desenvolvimento layout login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import loginComponent from './resources/login/loginComponent'
+import cadastroComponent from './resources/cadastro/cadastroComponent'
 
 Vue.config.productionTip = false
+Vue.use(
+  VueRouter,
+  BootstrapVue,
+  IconsPlugin,
+)
+
+const routes = [
+  { path: '/login', name: 'login', component: loginComponent },
+  { path: '/cadastro', name: 'cadastro', component: cadastroComponent },
+]
+
+const router = new VueRouter({routes})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/resources/cadastro/cadastroComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>Cadastro</p>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
